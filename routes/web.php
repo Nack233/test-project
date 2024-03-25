b38da16d5ef613f32d86ce50e51634c7f456e4f4
Nack233 <65160233@go.buu.ac.th>
2024-03-25 18:23:38 +0700

    okay

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\C_titles;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+Route::resource('titles', C_titles::class);
 
 Route::get('/', function () {
     return view('welcome');
